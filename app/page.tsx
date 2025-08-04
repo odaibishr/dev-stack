454cbd31e4ada2a634be39003b34374c5d4f5e12
Odai Bishr <odaibishr@gmail.com>
2025-08-04 22:24:00 +0300

    Config & Add Fontss

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 const Page = () => {
   return (
     <div>
-      <h1 className="text-2xl font-bold">Hello There👋</h1>
+      <h1 className="text-2xl font-bold">
+        Hello There In Inter Space Grotesk👋
+      </h1>
+      <h1 className="text-2xl font-bold font-space-grotesk">
+        Hello There In Space Grotesk 👋
+      </h1>
     </div>
   );
 };

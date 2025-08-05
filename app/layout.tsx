5e4966ca66c95722005b0e1d6af527fdf02466e9
Odai Bishr <odaibishr@gmail.com>
2025-08-05 20:57:35 +0300

    Setup Light & Dark, Theme, Shadcn, Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 
 import "./globals.css";
+import Navbar from "@/components/navigation/navbar";
 import ThemeProvider from "@/context/Theme";
 
 const inter = localFont({
@@ -37,6 +38,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <Navbar />
           {children}
         </ThemeProvider>
       </body>
---
+++ components/navigation/navbar/index.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+import Theme from "./theme";
+
+const Navbar = () => {
+  return (
+    <nav className="gap-5 flex-between background-light900_dark200 fixed z-50 w-full p-6 shadow-light-300 dark:shadow-none sm:px-12">
+      <Link href="/" className="flex flex-center gap-1">
+        <Image
+          src="/images/site-logo.svg"
+          width={23}
+          height={23}
+          alt="Devflow logo"
+        />
+        <p className="font-space-grotesk h2-bold text-dark-100 dark:text-light-900 max-sm:hidden ">
+          Dev<span className="text-primary-500">Flow</span>
+        </p>
+      </Link>
+
+      <p>Gloable Search</p>
+
+      <div className="flex-between gap-5">
+        <Theme />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;

9f015d533fb28aad0a5e5cad8ce474619e709718
Odai Bishr <odaibishr@gmail.com>
2025-09-25 23:32:12 +0300

    Implement home page header and placeholders for search, filters , and question cards

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,27 +1,31 @@
-import { auth, signOut } from "@/auth";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
 
 const Home = async () => {
-  const session = await auth();
-  console.log(session);
-
   return (
-    <div className="mt-20">
-      <h1 className="text-2xl font-bold">
-        Hello There In Inter Space Grotesk👋
-      </h1>
-      <h1 className="text-2xl font-bold font-space-grotesk">
-        Hello There In Space Grotesk 👋
-      </h1>
-      <form
-        action={async () => {
-          "use server";
-          await signOut({
-            redirectTo: ROUTES.SIGN_IN,
-          });
-        }}
-      ></form>
-    </div>
+    <>
+      <section className="flex w-full flex-col-reverse sm:flex-row justify-between gap-4 sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+
+        <Button
+          className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
+          asChild
+        >
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+        </Button>
+      </section>
+      <section className="mt-12">LocalSearch</section>
+      HomeFilter
+
+      <div className="mt-10 flex flex-col w-full gap-6">
+        <p>Question Card 1</p>
+        <p>Question Card 2</p>
+        <p>Question Card 3</p>
+        <p>Question Card 4</p>
+      </div>
+    </>
   );
 };
 
